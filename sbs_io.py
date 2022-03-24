a1b84dcb6b240e29282048fd3a2362942d50d59e
Ohashi_uhbie <tmatomatox1@gmail.com>
2022-03-25 02:39:40 +0900

    more readable codes

--- sbs_io.py
+++ sbs_io.py
@@ -1,3 +1,4 @@
+import copy
 class StringXY:
     def __init__(self, text, keys):
         self.text = text
@@ -28,45 +29,31 @@ class MojiretsuSousa:
         data.remove('\n')
         data.remove('\n')
         self.layout_list = data
-        self.layout_str = ''.join(map(str, data))
-
-        self.aru_nai()
+        self.nai_list = self.aru_nai()
     
     def aru_nai(self):
-        self.nai_list = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ;,./')
+        nai_list = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ;,./')
         for c in self.layout_list:
             if c != '_':
-                self.nai_list.remove(c)
-                self.nai_str = ''.join(map(str, self.nai_list))
+                nai_list.remove(c)
+        return nai_list
     
     def mizuni_modosu(self, target):
-        target = list(target)
-        self.modoshi = []
+        target = copy.deepcopy(target)
+        modoshi = []
         for i in range(30):
             c = self.layout_list[i]
             if c != '_':
-                self.modoshi.append(c)
+                modoshi.append(c)
             else:
                 item = target.pop(0)
-                self.modoshi.append(item)
+                modoshi.append(item)
         
-        self.modoshi = ''.join(map(str, self.modoshi))
-        return self.modoshi
-
-def dead_keys(abc):
-    res = []
-    abc_list = list(abc)
-    for x in abc_list:
-        if x in [';', ',', '.', '/']:
-            #res.append('_')
-            res.append(x)
-        else:
-            res.append(x)
-    return ''.join(map(str, res))
+        modoshi = ''.join(map(str, modoshi))
+        return modoshi
 
 if __name__ == '__main__':
     ms = MojiretsuSousa("config.txt")
     #print(ms.layout_list)
-    #print(ms.layout_str)
     print('nai:', ms.nai_str)
     print('modoshi:', ms.mizni_modosu('FGSDA'))
