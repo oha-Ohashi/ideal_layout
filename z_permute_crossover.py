3e8cee1ed720131d792314b831770e8708ad832b
Malkia <matomatox1@gmail.com>
2022-10-25 01:20:51 +0900

    10/25 partially mapped crossover done

--- z_permute_crossover.py
+++ z_permute_crossover.py
@@ -1,21 +1,30 @@
+import random
 p1 = list("987654321")
 p2 = list("246813579")
 
-def cross(p1, p2):
-	print(p1)
-	print(p2)
-	border = [2, 8]
+def partially_mapped_crossover(p1, p2):
 	child = [" " for _ in range(len(p1))]
-	mapindex = list(range(border[0], border[1]))
+	border = sorted([random.randint(0, len(p1)-1), random.randint(0, len(p1)-1)])
+	mapindex = list(range(border[0], border[1] + 1))
 	for i in mapindex:
 		child[i] = p2[i]
-	print(child)
 	for i in range(len(p1)):
-		if not p1[i] in child and not i in mapindex:
+		if not i in mapindex and not p1[i] in child:
 			child[i] = p1[i]
-	print(child)
-
+	for i in range(len(p1)):
+		if not p1[i] in child:
+			child[child.index(" ")] = p1[i]
 	return child
 
-cross(p1, p2)
-#print(cross(p1, p2))
+print(p1)
+print(p2)
+print(partially_mapped_crossover(p1, p2))
+print(partially_mapped_crossover(p1, p2))
+for i in range(1000):
+	partially_mapped_crossover(p1, p2)
+
+'''
+for i in range(20):
+	a = sorted([random.randint(8, 10), random.randint(8, 10)])
+	print(list(range(a[0], a[1] + 1)))
+'''
